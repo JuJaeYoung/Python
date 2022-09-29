787f04a29cf0005116f260972245ea0c71173073
JuJaeYoung <65886205+JuJaeYoung@users.noreply.github.com>
2022-09-29 10:46:05 +0900

    Delete Misson directory

--- Misson/Misson1 (22.09.27 ~ 22.09.29)/misson 7(주재영).py
+++
@@ -1,7 +0,0 @@
-s = input('문자를 입력하시오  ').upper()       # 대문자 : 65 ~ 90
-n = ord(s)                                  # e : 69
-
-for i in range(65,n+1) :
-    print(' ' * (ord(s) - n) + chr(i) * (n-64))
-    n -= 1
-    
