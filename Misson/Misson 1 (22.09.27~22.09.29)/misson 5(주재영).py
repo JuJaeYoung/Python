1218e1989122ad3bd50f885fc18b4d8149d8f5cc
JuJaeYoung <65886205+JuJaeYoung@users.noreply.github.com>
2022-09-29 10:44:24 +0900

    Delete Misson directory

--- Misson/Misson 1 (22.09.27~22.09.29)/misson 5(주재영).py
+++
@@ -1,9 +0,0 @@
-s = input()             # 입력 문자
-x = ord(s)              # 숫자로 변환
-
-for i in range(x,96,-1) :
-    lt = []                 # 입력 문자보다 순위가 낮은 알파벳 반환 리스트
-    for j in range(97,i+1) :
-        lt.append(chr(j))
-        
-    print("".join(lt))
--- Misson/Misson 1 (22.09.27~22.09.29)/misson 6(주재영).py
+++
@@ -1,3 +0,0 @@
-n = int(input('숫자(홀수)를 입력하시오  '))
-for i in range(1, n+1,2) :
-    print(' ' * int((n - i) / 2) + '*' * i + ' ' * int((n - i) / 2))    
